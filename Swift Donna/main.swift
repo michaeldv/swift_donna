c4f9534798636b230481df3e107fa4dbf541e965
Michael Dvorkin <mike@dvorkin.net>
2014-10-25 12:33:38 -0700

    Initial Commit

---
+++ Swift Donna/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Donna
+//
+//  Created by Michael Dvorkin on 10/25/14.
+//  Copyright (c) 2014 Michael Dvorkin. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
