ee28735f9e05bfba8e2f2734acd50d3d3d84a221
Michael Dvorkin <mike@dvorkin.net>
2014-12-24 21:52:58 -0800

    Bitmask.last()

--- SwiftDonna/bitmask.swift
+++ SwiftDonna/bitmask.swift
@@ -19,6 +19,28 @@ public extension Bitmask {
         return Int((mask * 0x0101010101010101) >> 56)
     }
 
+    // Eugene Nalimov's bitScanReverse: finds most significant bit set (MSB):
+    var last: Int {
+        var mask = self, offset = 0
+
+        if mask > 0xFFFFFFFF {
+            mask >>= 32
+            offset = 32
+        }
+
+        if mask > 0xFFFF {
+            mask >>= 16
+            offset += 16
+        }
+
+        if mask > 0xFF {
+            mask >>= 8
+            offset += 8
+        }
+
+        return offset + msbLookup[Int(mask)]
+    }
+
     func on(offset: Int) -> Bool {
         return (self & Bitmask(1 << offset)) != 0
     }
--- SwiftDonna/init.swift
+++ SwiftDonna/init.swift
@@ -1,2 +1,27 @@
+var msbLookup: [Int] = []
+
 public func initialize() {
+    func initArrays() {
+        println("initArrays")
+        // Most significant bit lookup table.
+        for i in 0..<msbLookup.count {
+            if i > 127 {
+                msbLookup[i] = 7
+            } else if i > 63 {
+                msbLookup[i] = 6
+            } else if i > 31 {
+                msbLookup[i] = 5
+            } else if i > 15 {
+                msbLookup[i] = 4
+            } else if i > 7 {
+                msbLookup[i] = 3
+            } else if i > 3 {
+                msbLookup[i] = 2
+            } else if i > 1 {
+                msbLookup[i] = 1
+            }
+        }
+    }
+
+    initArrays()
 }
